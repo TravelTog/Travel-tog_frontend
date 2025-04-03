5443fdaf5d0d19294d0d72c5fe0a8b1d03647d66
Churchkhelass <n.kochnev.45@mail.ru>
2025-04-03 12:03:19 +0400

    change rout page & new page

--- TravelTog/Pages/ProfileLogin/Login.tsx
+++ TravelTog/Pages/ProfileLogin/Login.tsx
@@ -3,21 +3,21 @@ import { View, Text, TextInput, Button } from 'react-native';
 import { AuthContext } from '../../context/AuthContext';
 
 const Login = ({ navigation }) => {
-  const authContext = useContext(AuthContext);
+   const authContext = useContext(AuthContext);
 
-  const handleLogin = () => {
-    authContext?.login();
-  };
+   const handleLogin = () => {
+      authContext?.login();
+   };
 
-  return (
-    <View>
-      <Text>Login</Text>
-      <TextInput placeholder="Email" style={{ borderWidth: 1, marginBottom: 10 }} />
-      <TextInput placeholder="Password" secureTextEntry style={{ borderWidth: 1, marginBottom: 10 }} />
-      <Button title="Login" onPress={handleLogin} />
-      <Button title="Register" onPress={() => navigation.navigate('Register')} />
-    </View>
-  );
+   return (
+      <View>
+         <Text>Login</Text>
+         <TextInput placeholder="Email" style={{ borderWidth: 1, marginBottom: 10 }} />
+         <TextInput placeholder="Password" secureTextEntry style={{ borderWidth: 1, marginBottom: 10 }} />
+         <Button title="Login" onPress={handleLogin} />
+         <Button title="Register" onPress={() => navigation.navigate('Register')} />
+      </View>
+   );
 };
 
 export default Login;
